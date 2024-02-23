851858fc8c9df348576f9fc247aded6a78fdc3b6
sari <emre242469@gmail.com>
2024-02-23 17:18:49 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+class MedlineScraper:
+    def __init__(self):
+        self.base_url = "https://scholar.google.com/scholar?hl=tr&as_sdt=0%2C5&q="
+
+@app.route('/')
+def hello_world():
+    return render_template('main.html')
+
+
+# main driver function
+if __name__ == '__main__':
+    app.run()
