a48b0e99d7b14e2557b504cf8e7a5f76c95aa2d3
sari <emre242469@gmail.com>
2024-02-23 23:56:57 +0300

    link oluşturuyor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,11 +6,21 @@ class MedlineScraper:
     def __init__(self):
         self.base_url = "https://scholar.google.com/scholar?hl=tr&as_sdt=0%2C5&q="
 
-@app.route('/')
+@app.route('/search-yap/<string:search>')
+def search_yap(search):
+    scraper = MedlineScraper()
+    search = search.replace(" ","+")
+    search_url = scraper.base_url + search
+    print(search_url)
+    return redirect(search_url)
+
+@app.route('/',methods=['GET','POST'])
 def hello_world():
-    return render_template('main.html')
+    if request.method == 'POST':
+        search = request.form['search']
+        return redirect(url_for('search_yap', search=search))
+    else:
+        return render_template('main.html')
 
-
-# main driver function
 if __name__ == '__main__':
     app.run()
