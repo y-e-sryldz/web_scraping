6b2e09277f263d69ac014ada2dc679b2313a6063
sari <emre242469@gmail.com>
2024-03-05 16:03:51 +0300

    mongodb koleks,yon adı kaydetme okey

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import requests
 from bs4 import BeautifulSoup
-import pymongo
 from pymongo import MongoClient
 
-connect = MongoClient("mongodb+srv://nevasarac:p8VUTFzTom0ANOxC@atlascluster.gh1liqu.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
-database = connect["Deneme"]
-collection = database["makale"]
-
-ornek = {"Yayın id":"a1","Yayın adı":"yazılım"}
-collection.insert_one(ornek)
-
-
 app = Flask(__name__)
+
 
 class MedlineScraper:
     def __init__(self):
         self.base_url = "https://scholar.google.com/scholar?hl=tr&as_sdt=0%2C5&q="
+
+
+def create_search_collection(search):
+    connect = MongoClient(
+        "mongodb+srv://nevasarac:p8VUTFzTom0ANOxC@atlascluster.gh1liqu.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
+    database = connect["Deneme"]
+    search = search.replace("+", " ")
+    if "search" not in database.list_collection_names():
+        collection = database[search]
+        return collection
+    else:
+        return database[search]
+
+
+def save_search_results(results,search):
+    collection = create_search_collection(search)
+    for result in results:
+        collection.insert_one(result)
+
 
 @app.route('/search-yap/<string:search>')
 def search_yap(search):
@@ -39,8 +50,10 @@ def search_yap(search):
                 pdf_link = pdf_item.find('a', href=True)
                 pdf_link_text = pdf_link.text.strip()
                 pdf_link_url = pdf_link['href']
-                search_results.append({'link_text': link_text, 'link_url': link_url, 'pdf_link_text': pdf_link_text, 'pdf_link_url': pdf_link_url})
+                search_results.append({'link_text': link_text, 'link_url': link_url, 'pdf_link_text': pdf_link_text,
+                                       'pdf_link_url': pdf_link_url})
 
+            save_search_results(search_results,search)  # Search sonuçlarını MongoDB'ye kaydet
             return render_template('search_results.html', results=search_results)
         else:
             return render_template('search_results.html', results=None)
@@ -48,13 +61,14 @@ def search_yap(search):
         return render_template('search_results.html', results=None)
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
         search = request.form['search']
         return redirect(url_for('search_yap', search=search))
     else:
         return render_template('main.html')
+
 
 if __name__ == '__main__':
     app.run()
